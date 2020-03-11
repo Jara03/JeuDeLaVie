e27723903aa91458247ad8d501132c102621427f
paullouppe <paullouppe@gmail.com>
2020-03-11 09:45:28 +0100

    tg

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+H
+src/main/scala/yow.scala,d\0\d0454a58737fd68f64a4d7f29cb7f211536bcd02
---
+++ src/main/scala/yow.scala
@@ -0,0 +1,48 @@
+//Partie 1
+type Grille = List[(Int, Int)]
+
+//--------------------------------------------------------
+//Partie 2
+
+//Convertit une liste de String en Grille
+def chaineToGrille(l:List[String]): Grille =  {
+
+}
+
+//Affiche la grille à l'écran
+def afficherGrille(g: Grille): Unit = {
+
+}
+
+
+//--------------------------------------------------------
+//Partie 3
+
+//retourne les 8 voisines
+def voisines8(l:Int, c:Int): List[(Int, Int)] = {
+
+}
+
+//retourne la liste des cellules qui survivent
+// à l'étape suivante selon les règles
+def survivantes(g:Grille):Grille = {
+
+}
+
+//liste des cellules mortes voisines (suceptibles de naître)
+def candidates(g:Grille):Grille = {
+
+}
+
+//listes des cellules qui naissent à l'étape suivante
+def naissances(g:Grille):Grille = {
+
+}
+
+//grille initialise affiche en fonction d'un nb d'étapes n et
+// affiche n itérations de la simulation
+def jeuDeLaVie(init:Grille, n:Int):Unit = {
+
+}
+
+
